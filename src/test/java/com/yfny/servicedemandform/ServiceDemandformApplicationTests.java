c39c318bf2f8cfc991c739e5fdf242e6d12c3673
jisongzhou <568777035@qq.com>
2019-03-29 09:40:40 +0800

    配置修正

--- src/test/java/com/yfny/servicedemandform/ServiceDemandformApplicationTests.java
+++ src/test/java/com/yfny/servicedemandform/ServiceDemandformApplicationTests.java
@@ -1,9 +1,17 @@
 package com.yfny.servicedemandform;
 
+import com.yfny.utilscommon.generator.entity.RequestInfo;
+import com.yfny.utilscommon.generator.invoker.ProducerInvoker;
+import com.yfny.utilscommon.generator.invoker.TestInvoker;
+import com.yfny.utilscommon.generator.invoker.base.Invoker;
+import com.yfny.utilscommon.util.CodeInfoUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.MethodParameter;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +19,35 @@ public class ServiceDemandformApplicationTests {
 
     @Test
     public void contextLoads() {
+        List<RequestInfo> requestInfoList = CodeInfoUtils.getRequestInfos();
+        for (RequestInfo requestInfo : requestInfoList) {
+            System.out.println("****************************************************");
+            System.out.println("url: " + requestInfo.getUrl());
+            System.out.println("methodName: " + requestInfo.getMethodName());
+            System.out.println("requestMethod: " + requestInfo.getRequestMethod());
+            String requestMethod = requestInfo.getRequestMethod();
+            for (MethodParameter parameter : requestInfo.getRequestParams()) {
+                System.out.println("requestParams: " + parameter.getParameter().getName() + "  requestParamsType: " + parameter.getParameter().getType());
+            }
+            System.out.println("****************************************************");
+        }
+    }
+    
+    @Test
+    public void producerInvokerTest() {
+        Invoker invoker = new ProducerInvoker.Builder()
+                .setClassName("Xqdxx")
+                .setDescription("需求单详情")
+                .setFirst(true)
+                .build();
+        invoker.execute();
+    }
+
+    @Test
+    public void initUnitTest() {
+        Invoker invoker = new TestInvoker.Builder()
+                .build();
+        invoker.execute();
     }
 
 }
